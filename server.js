05ad2e2b2c80114cd9317ea8c5fa66b90868409f
John Clark <johngclark36@gmail.com>
2016-12-15 21:16:43 +0000

    Add a route for home and for when a parameter is entered, return a json response object containing the date in natural and unix form

--- server.js
+++ server.js
@@ -2,9 +2,40 @@ var express = require('express')
 
 var app = express();
 
-
 app.get('/', function (req, res) {
-    res.send("Hello World!")
+    res.send('Welcome to the Timestamp microservice')
+})
+
+
+app.get('/:date', function (req, res) {
+    var params = req.params.date
+    console.log(params)
+    if (Number.isInteger(parseInt(params))) {
+        var milliseconds = params * 1000;
+        var date = new Date (milliseconds)
+        var json = {
+            'unix': params,
+            'natural': date.toDateString()
+        }
+        console.log(JSON.stringify(json))
+        res.json(json)
+    
+    } else {
+        var naturalDate = new Date(params)
+        if (naturalDate.toDateString() !== 'Invalid Date') {
+            var unix = naturalDate.getTime() / 1000
+            var json = {
+                'unix': unix,
+                'natural': naturalDate
+            }
+            res.json(json)
+        } else {
+            res.json({ 'unix': null, 'natural': null })
+        }
+    
+    }
+
+    
 })
 
 app.listen('8080')
